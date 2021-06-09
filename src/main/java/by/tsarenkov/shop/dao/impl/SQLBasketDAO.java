d4a814cfaee61e424903153626d3ead04f26143d
Viachalau <tsarenkovslava@yandex.ru>
2021-06-09 09:49:44 +0300

    feat

--- src/main/java/by/tsarenkov/shop/dao/impl/SQLBasketDAO.java
+++ src/main/java/by/tsarenkov/shop/dao/impl/SQLBasketDAO.java
@@ -2,10 +2,14 @@ package by.tsarenkov.shop.dao.impl;
 
 import by.tsarenkov.shop.dao.BasketDAO;
 import by.tsarenkov.shop.dao.ProductDAO;
+import by.tsarenkov.shop.dao.db.ConnectionPool;
 
 import java.util.ArrayList;
 
 public class SQLBasketDAO implements BasketDAO {
+
+    private final static ConnectionPool pool = ConnectionPool.getInstance();
+
     @Override
     public ArrayList<ProductDAO> getAllProducts() {
         return null;
--- src/main/java/by/tsarenkov/shop/service/impl/OrderServiceImpl.java
+++ src/main/java/by/tsarenkov/shop/service/impl/OrderServiceImpl.java
@@ -1,11 +1,16 @@
 package by.tsarenkov.shop.service.impl;
 
 import by.tsarenkov.shop.bean.Order;
+import by.tsarenkov.shop.dao.BasketDAO;
+import by.tsarenkov.shop.dao.DAOProvider;
+import by.tsarenkov.shop.dao.OrderDAO;
 import by.tsarenkov.shop.service.OrderService;
 
 import java.util.ArrayList;
 
 public class OrderServiceImpl implements OrderService {
+    private static final DAOProvider provider = DAOProvider.getInstance();
+    private static final OrderDAO orderDAO = provider.getOrderDAO();
 
     @Override
     public boolean addNewOrder(Order order) {
